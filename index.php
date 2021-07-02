08a415760d8a76814d965498fd912f9829d72e39
AYOUB-lec <63780707+AYOUB-lec@users.noreply.github.com>
2021-07-02 10:02:04 +0100

    Update index.php
    zid hadi

--- index.php
+++ index.php
@@ -4,28 +4,10 @@ if(isset($_POST['submit'])){
 $name = $_POST['name'];
 }
 
+echo '<h1>hello $name </h1>'
 
 
 ?>
 
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./index.css">
-    <title>Document</title>
-</head>
-<body>
-    <form action="#" method="POST">
-     <input type="text" placeholder="your name" name="name">
-    <input type="submit" name="submit">
-    </form>
-    <h1><?php echo 'hello '.$name ?></php></h1>
-
-</body>
-
-</html>
